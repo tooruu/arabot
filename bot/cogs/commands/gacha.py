9e0173a7cf98bd1308ce68e0380fc0421d4095df
tooru <sergunkoudrin@inbox.lv>
2020-05-25 01:52:48 +0300

    separate gacha pool from code

--- bot/cogs/commands/gacha.py
+++ bot/cogs/commands/gacha.py
@@ -1,5 +1,6 @@
 from random import choice, choices
 from discord.ext.commands import command, Cog, cooldown, BucketType, CommandOnCooldown
+from ...res.gacha import pool
 
 class Gacha(Cog):
 	def __init__(self, client):
@@ -18,41 +19,6 @@ class Gacha(Cog):
 		"UpMats": .1474,
 		"EquEXP": .1474,
 		"Coins": .0737,
-	}
-
-	pool = {
-		"dorm": {
-		"ValkS": [
-		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
-		"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
-		],
-		"ValkA": [
-		"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
-		"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
-		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
-		],
-		"ValkB": ["Valkyrie Chariot", "Battle Storm", "White Comet", "Crimson Impulse"],
-		"FragS": [
-		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
-		"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
-		],
-		"FragA": [
-		"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
-		"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
-		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
-		],
-		"Weap4": ["Keys of the Void", "Key of Reason", "Blood Dance", "Hekate's Gloom", "Oath of Judah"],
-		"Weap3": ["Azure Storm", "Nitro Crystal", "Dark Crusher"],
-		"Stig4": ["Sirin", "Shakespeare", "Rasputin", "Nobel", "Beethoven", "Caravaggio", "Michelangelo", "Jin Shengtan"],
-		"Stig3": [
-		"Charlemagne", "Elizabeth Bathory", "Galileo", "Ryunosuke Akutagawa", "Roald Amundsen", "Nikola Tesla", "Edison",
-		"Attila", "Sakamoto Ryouma", "Ogier", "Shigure Kira", "Rinaldo", "Tchaikovsky", "Scott", "Wang Zhaojun"
-		],
-		"UpMats":
-		["Super EXP Chip", "ADV EXP Chip", "ADV Learning Chip", "Basic Learning Chip", "Microreactor", "Phase Shifter"],
-		"EquEXP": ["Twin Soul Crystal", "Soul Crystal", "Twin Soul Shard", "Soul Shard"],
-		"Coins": ["HOMEI Chest", "HOMU Chest", "HOLI Chest", "HOWO Chest", "HOLA Chest", "HOMI Chest"],
-		}
 	}
 
 	@cooldown(1, 60, BucketType.user)
---
+++ bot/res/gacha.py
@@ -0,0 +1,37 @@
+pool = {
+	"dorm": {
+	"ValkS": [
+	"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
+	"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
+	],
+	"ValkA": [
+	"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
+	"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
+	"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
+	],
+	"ValkB": ["Valkyrie Chariot", "Battle Storm", "White Comet", "Crimson Impulse"],
+	"FragS": [
+	"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
+	"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
+	],
+	"FragA": [
+	"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
+	"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
+	"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
+	],
+	"Weap4": [
+	"Keys of the Void", "Key of Reason", "Blood Dance", "Hekate's Gloom", "Oath of Judah", "Dawn Break: Talwar",
+	"Sakura Blossom", "Undine's Tale", "Cinder Hawk"
+	],
+	"Weap3": ["Azure Storm", "Nitro Crystal", "Dark Crusher"],
+	"Stig4": ["Sirin", "Shakespeare", "Rasputin", "Nobel", "Beethoven", "Caravaggio", "Michelangelo", "Jin Shengtan"],
+	"Stig3": [
+	"Charlemagne", "Elizabeth Bathory", "Galileo", "Ryunosuke Akutagawa", "Roald Amundsen", "Nikola Tesla", "Edison",
+	"Attila", "Sakamoto Ryouma", "Ogier", "Shigure Kira", "Rinaldo", "Tchaikovsky", "Scott", "Wang Zhaojun"
+	],
+	"UpMats":
+	["Super EXP Chip", "ADV EXP Chip", "ADV Learning Chip", "Basic Learning Chip", "Microreactor", "Phase Shifter"],
+	"EquEXP": ["Twin Soul Crystal", "Soul Crystal", "Twin Soul Shard", "Soul Shard"],
+	"Coins": ["HOMEI Chest", "HOMU Chest", "HOLI Chest", "HOWO Chest", "HOLA Chest", "HOMI Chest"],
+	}
+}
