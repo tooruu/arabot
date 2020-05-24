c935aab1c2ba693379d4e519e0dd76b7670ff1ad
tooru <sergunkoudrin@inbox.lv>
2020-05-25 00:51:01 +0300

    update python, update gacha

--- bot/cogs/commands/gacha.py
+++ bot/cogs/commands/gacha.py
@@ -1,7 +1,6 @@
 from random import choice, choices
 from discord.ext.commands import command, Cog, cooldown, BucketType, CommandOnCooldown
 
-
 class Gacha(Cog):
 	def __init__(self, client):
 		self.bot = client
@@ -24,43 +23,33 @@ class Gacha(Cog):
 	pool = {
 		"dorm": {
 		"ValkS": [
-		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean",
-		"Goushinnso Memento", "Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress",
-		"Dimensional Breaker", "Violet Executer"
+		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
+		"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
 		],
 		"ValkA": [
 		"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
 		"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
-		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper",
-		"Scarlet Fusion"
+		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
 		],
 		"ValkB": ["Valkyrie Chariot", "Battle Storm", "White Comet", "Crimson Impulse"],
 		"FragS": [
-		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean",
-		"Goushinnso Memento", "Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress",
-		"Dimensional Breaker", "Violet Executer"
+		"Celestial Hymn", "Blood Rose", "Shadow Knight", "Herrscher of the Void", "Knight Moonbean", "Goushinnso Memento",
+		"Black Nucleus", "Sixth Serenade", "Phoenix", "Lightning Empress", "Dimensional Breaker", "Violet Executer"
 		],
 		"FragA": [
 		"Luna Kindred", "Valkyrie Accipiter", "Valkyrie Pledge", "Sakuno Rondo", "Night Squire", "Flame Sakitama",
 		"Wolf's Dawn", "Ritual Imayoh", "Gyakushin Miko", "Shadow Dash", "Valkyrie Bladestrike", "Valkyrie Ranger",
-		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper",
-		"Scarlet Fusion"
+		"Divine Prayer", "Yamabuki Armor", "Valkyrie Triumph", "Arctic Kriegsmesser", "Snowy Sniper", "Scarlet Fusion"
 		],
-		"Weap4": ["Keys of the Void", "Path to Acheron", "Key of Reason", "Blood Dance", "Hekate's Gloom"],
+		"Weap4": ["Keys of the Void", "Key of Reason", "Blood Dance", "Hekate's Gloom", "Oath of Judah"],
 		"Weap3": ["Azure Storm", "Nitro Crystal", "Dark Crusher"],
-		"Stig4": [
-		"Sirin", "Shakespeare", "Rasputin", "Nobel", "Beethoven", "Caravaggio", "Michelangelo", "Oath of Judah",
-		"Jin Shengtan"
-		],
+		"Stig4": ["Sirin", "Shakespeare", "Rasputin", "Nobel", "Beethoven", "Caravaggio", "Michelangelo", "Jin Shengtan"],
 		"Stig3": [
-		"Charlemagne", "Elizabeth Bathory", "Galileo", "Ryunosuke Akutagawa", "Roald Amundsen", "Nikola Tesla",
-		"Edison", "Attila", "Sakamoto Ryouma", "Ogier", "Shigure Kira", "Rinaldo", "Tchaikovsky", "Scott",
-		"Wang Zhaojun"
+		"Charlemagne", "Elizabeth Bathory", "Galileo", "Ryunosuke Akutagawa", "Roald Amundsen", "Nikola Tesla", "Edison",
+		"Attila", "Sakamoto Ryouma", "Ogier", "Shigure Kira", "Rinaldo", "Tchaikovsky", "Scott", "Wang Zhaojun"
 		],
-		"UpMats": [
-		"Super EXP Chip", "ADV EXP Chip", "ADV Learning Chip", "Basic Learning Chip", "Microreactor",
-		"Phase Shifter"
-		],
+		"UpMats":
+		["Super EXP Chip", "ADV EXP Chip", "ADV Learning Chip", "Basic Learning Chip", "Microreactor", "Phase Shifter"],
 		"EquEXP": ["Twin Soul Crystal", "Soul Crystal", "Twin Soul Shard", "Soul Shard"],
 		"Coins": ["HOMEI Chest", "HOMU Chest", "HOLI Chest", "HOWO Chest", "HOLA Chest", "HOMI Chest"],
 		}
@@ -70,10 +59,15 @@ class Gacha(Cog):
 	@command()
 	async def gacha(self, ctx, supply="dorm", pulls: int = 10):
 		types = choices([*self.rates.keys()], self.rates.values(), k=pulls)
-		drops = [
-			choice(self.pool[supply.lower()][i])
-			if "Stig" not in i else f"{choice(self.pool[supply.lower()][i])} ({choice(('T', 'M', 'B'))})" for i in types
-		]
+		drops = []
+		for typ in types:
+			drop = choice(self.pool[supply.lower()][typ])
+			if "Stig" in typ:
+				drop += f" ({choice(('T', 'M', 'B'))})"
+			if typ in ("ValkS", "Weap4", "Stig4"):
+				drop = f"**{drop}**"
+			if "Frag" in typ:
+				drop += f" frags x{choice((3,4,5,6))}"
 		await ctx.send("__Your **{}** supply drops:__\n{}".format(supply.capitalize(), "\n".join(drops)))
 
 	@gacha.error
@@ -82,7 +76,6 @@ class Gacha(Cog):
 			return
 		self.gacha.reset_cooldown(ctx)
 		raise error
-
 
 def setup(client):
 	client.add_cog(Gacha(client))
