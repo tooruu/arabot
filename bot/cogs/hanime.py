85f3d50cf48d1f4bfd5861c0c3d4575afc9e3d94
tooru <sergunkoudrin@inbox.lv>
2020-05-25 05:25:34 +0300

    gaychan

--- bot/cogs/hanime.py
+++ bot/cogs/hanime.py
@@ -1,7 +1,7 @@
 from discord.ext.commands import Cog
 from discord.ext.tasks import loop
 from discord import Embed
-import cfscrape
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class HAnime(Cog):
@@ -9,28 +9,30 @@ class HAnime(Cog):
 		self.bot = client
 		self.channel = None
 		self.latest = None
+		self.driver = webdriver.PhantomJS()
 
 	@loop(hours=1)
 	async def check(self):
-		connection = cfscrape.create_scraper().get("https://hanime.tv")
-		if connection.status_code == 200:
-			hanime = BeautifulSoup(connection.content.decode("utf-8"), "html.parser")
-			new_release = hanime.find_all(class_="htv-carousel__slider")[1][0]
-			title = new_release.find(class_="hv-title").string.strip()
-			if title == self.latest:
-				return
-			self.latest = title
-			link = new_release.a["href"]
-			cover = new_release.find("img")["src"]
-			await self.channel.send(
-				"Ara-ara, new release on HAnime! <:RitaPleasure:676909760158629890>",
-				embed=Embed().set_author(name=title, url="https://hanime.tv" + link).set_thumbnail(url=cover)
-			)
+		#connection = cfscrape.create_scraper().get("https://hanime.tv")
+		#if connection.status_code == 200:
+		#	hanime = BeautifulSoup(connection.content.decode("utf-8"), "html.parser")
+		#	new_release = hanime.find_all(class_="htv-carousel__slider")[1][0]
+		#	title = new_release.find(class_="hv-title").string.strip()
+		#	if title == self.latest:
+		#		return
+		#	self.latest = title
+		#	link = new_release.a["href"]
+		#	cover = new_release.find("img")["src"]
+		#	await self.channel.send(
+		#		"Ara-ara, new release on HAnime! <:RitaPleasure:676909760158629890>",
+		#		embed=Embed().set_author(name=title, url="https://hanime.tv" + link).set_thumbnail(url=cover)
+		#	)
 
 	@Cog.listener()
 	async def on_ready(self):
-		self.channel = self.bot.get_channel(676893776840753155)
+		self.channel = self.bot.get_channel(676953593148080148) #lewd: 676893776840753155
 		#self.check.start() TODO: Account for lazy loading
 
 def setup(client):
-	client.add_cog(HAnime(client))
+	#client.add_cog(HAnime(client))
+	pass
