fc29c6e36368dbfcf419d85c5727750fad318f7b
tooru <sergunkoudrin@inbox.lv>
2020-05-27 16:25:48 +0300

    fix TMB not showing again

--- bot/cogs/commands/gacha.py
+++ bot/cogs/commands/gacha.py
@@ -5,12 +5,16 @@ from json import load
 class Gacha(Cog):
 	def __init__(self, client):
 		self.bot = client
-		with open("./bot/res/gacha_pool.json") as pool:
+		with open("./bot/res/gacha_pool.json") as gacha:
 			self.pool = {
+				supply: {
 				typ: [stig if '(' in stig else f"{stig} ({slot})" for slot in ('T', 'M', 'B')
 				for stig in items] if "Stig" in typ else items
-				for typ, items in load(pool).items()
+				for typ, items in pool.items()
+				}
+				for supply, pool in load(gacha).items()
 			}
+			print(self.pool)
 		with open("./bot/res/gacha_rates.json") as rates:
 			self.rates = load(rates)
 
@@ -19,10 +23,10 @@ class Gacha(Cog):
 	async def gacha(self, ctx, supply="dorm", pulls: int = 10):
 		if not self.pool.get(supply := supply.lower()):
 			await ctx.send("Invalid supply")
-			return
+			raise TypeError
 		if pulls < 1:
 			await ctx.send("Invalid amount")
-			return
+			raise ValueError
 		pulls = min(pulls, 10)
 		types = choices([*self.rates[supply]], self.rates[supply].values(), k=pulls)
 		drops = []
