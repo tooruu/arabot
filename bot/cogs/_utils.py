c500cd146dc34fb8bdb4b752f25b6264148e18a7
tooru <sergunkoudrin@inbox.lv>
2020-09-10 16:54:56 +0300

    dont trigger ;ud on quotes

--- bot/cogs/_utils.py
+++ bot/cogs/_utils.py
@@ -9,7 +9,7 @@ from os import environ
 BOT_DEBUG = False
 BOT_NAME = "AraBot"
 BOT_PREFIX = "-" if BOT_DEBUG else ";"
-BOT_VERSION = "1.6.6" #TODO: UPDATE!
+BOT_VERSION = "1.6.7" #TODO: UPDATE!
 if BOT_DEBUG:
 	BOT_VERSION += " (DEBUG MODE)"
 
