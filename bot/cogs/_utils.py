a82ed0fa7fa4b45beee170a9944a46dfc7fd9392
tooru <sergunkoudrin@inbox.lv>
2020-10-08 19:16:15 +0300

    oops

--- bot/cogs/_utils.py
+++ bot/cogs/_utils.py
@@ -7,7 +7,7 @@ from discord.utils import find
 from discord import Status, Activity
 from os import environ
 
-BOT_DEBUG = True
+BOT_DEBUG = False
 BOT_NAME = "AraBot"
 BOT_PREFIX = "-" if BOT_DEBUG else ";", "ara "
 BOT_VERSION = "1.7.7" #TODO: UPDATE!
