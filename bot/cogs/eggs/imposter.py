cbe30b48d56db677d630d440587d791b4267a721
tooru <40900803+tooruu@users.noreply.github.com>
2020-10-11 15:00:51 +0300

    add cd

--- bot/cogs/eggs/imposter.py
+++ bot/cogs/eggs/imposter.py
@@ -1,14 +1,16 @@
 from discord.ext.commands import Cog
 from re import search
-from asyncio import wait_for, TimeoutError
+from asyncio import wait_for, TimeoutError, sleep
 
 class EasterEggs(Cog):
 	def __init__(self, client):
 		self.bot = client
+		self.running = False
 
 	@Cog.listener("on_message")
 	async def imposter(self, msg):
-		if not msg.author.bot and msg.author.voice and (chl:=msg.author.voice.channel) and (word:=search("\\b(impost[eo]r)\\b", msg.content.lower())):
+		if not self.running not msg.author.bot and msg.author.voice and (chl:=msg.author.voice.channel) and (word:=search("\\b(impost[eo]r)\\b", msg.content.lower())):
+			self.running = True
 			TIMEOUT = 30
 			word = word.group(1)
 			await msg.channel.send("<:KonoDioDa:676949860502732803>")
@@ -31,6 +33,8 @@ class EasterEggs(Cog):
 				await imposter.move_to(None, reason="The " + word)
 			else:
 				await msg.channel.send("No one was ejected.")
+			sleep(60)
+			self.running = False
 
 def setup(client):
 	client.add_cog(EasterEggs(client))
