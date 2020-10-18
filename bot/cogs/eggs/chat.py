ec40916a755cf5aaf4e580f2964e4f751e7269a5
tooru <sergunkoudrin@inbox.lv>
2020-10-18 21:49:27 +0300

    fix very funny meme

--- bot/cogs/eggs/chat.py
+++ bot/cogs/eggs/chat.py
@@ -7,17 +7,17 @@ class Chat(Cog, name="Eggs"):
 		self.bot = client
 
 	@Cog.listener("on_message")
-	async def who_listener(self, msg):
+	async def who(self, msg):
 		if len(msg.content) < 20 and is_valid(self.bot, msg, "^who\\b"):
 			await msg.channel.send("ur mom")
 
 	@Cog.listener("on_message")
-	async def im_hi_listener(self, msg):
+	async def im_hi(self, msg):
 		if len(msg.content) < 30 and (regex := match(r"(?:i(?:['’]?m|\sam)\s)+(.+)", msg.content.lower())):
 			await msg.channel.send(f"hi {regex.group(1)}\nim {BOT_NAME}")
 
 	@Cog.listener("on_message")
-	async def im_hi_listener(self, msg):
+	async def racing(self, msg):
 		if is_valid(self.bot, msg, "\\brac(?:e|ing)\\b"):
 			await msg.channel.send("***RACING TIME***")
 			await msg.channel.send("🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽🧑🏼‍🦽")
