a194a20d0b17cae965be07a21e807e2590743082
tooru <sergunkoudrin@inbox.lv>
2020-10-18 07:15:50 +0300

    make Finder clearer, find is now async

--- bot/cogs/_utils.py
+++ bot/cogs/_utils.py
@@ -12,7 +12,7 @@ from re import search
 BOT_DEBUG = False
 BOT_NAME = "AraBot"
 BOT_PREFIX = ("-",) if BOT_DEBUG else (";", "ara ")
-BOT_VERSION = "2.5.0"
+BOT_VERSION = "2.5.1"
 # 1.0.0 major changes
 # 0.1.0 new features
 # 0.0.1 minor improvements & bugfixes
@@ -24,53 +24,49 @@ is_dev = lambda ctx: ctx.author.id in (337343326095409152, 447138372121788417, 4
 is_valid = lambda client, msg, expr="": [msg.content.startswith(pfx) for pfx in client.command_prefix].count(True) == 0 and msg.author != client.user and not msg.author.bot and search(expr, msg.content.lower())
 
 async def set_presence(client, _type: int, name: str, _status: Status = None):
-	if isinstance(_status, Status):
-		await client.change_presence(status=_status, activity=Activity(name=name, type=_type))
-	else:
-		await client.change_presence(activity=Activity(name=name, type=_type))
+	await client.change_presence(status=_status if isinstance(_status, Status) else None, activity=Activity(name=name, type=_type))
 
 class Finder(Converter):
 	"""
-	Abstract class to convert *argument* to desired class.
+	Abstract class to convert *argument* to desired type.
 	1.	Try to convert via subclass's parents'
 		`convert` methods (implemented in discord.py)
-	2. Try to convert via subclass's method `find`
+	2. Try to convert via subclass's own method `find`
 
 	# Example usage:
 	class FindObject(Finder, AConverter, BConverter):
 		@staticmethod
-		def find(ctx, argument):
-			return utils.find(lambda obj: obj.att == argument, ctx.obj_list)
+		async def find(ctx, argument):
+			return utils.find(lambda obj: obj.att == argument, ctx.obj_list) or await thing()
 	"""
 	def __init__(self):
-		if len(type(self).__bases__) == 1:
-			raise IndexError(f"<'{type(self).__name__}'> must have at least 2 parents")
-		for base in type(self).__bases__[1:]:
-			if not issubclass(base, Converter):
-				raise TypeError(f"Parents of <'{type(self).__name__}'> are not derived from Converter")
+		if len(self.__class__.__bases__) == 1:
+			raise IndexError(f"<'{self.__class__.__name__}'> must have at least 2 parents")
+		if not all(issubclass(base, Converter) for base in self.__class__.__bases__[1:]):
+			raise TypeError(f"Parents of <'{self.__class__.__name__}'> are not derived from Converter")
 
 	async def convert(self, ctx, argument):
-		for conv in type(self).__bases__[1:]:
+		for conv in self.__class__.__bases__[1:]:
 			try:
 				return await conv().convert(ctx, argument)
 			except BadArgument:
 				pass
-		return self.find(ctx, argument)
+		return await self.find(ctx, argument)
 
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		raise NotImplementedError("Derived classes need to implement this.")
 
 class FindMember(Finder, MemberConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		return find(
 			lambda member: not member.bot and member.display_name.lower().startswith(argument.lower()), ctx.guild.members
 		)
 
 class FindEmoji(Finder, EmojiConverter, PartialEmojiConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		ctx.bot.guilds.insert(0, ctx.bot.guilds.pop(ctx.bot.guilds.index(ctx.guild)))
 		for guild in ctx.bot.guilds:
 			if emote := find(
@@ -83,24 +79,24 @@ class FindEmoji(Finder, EmojiConverter, PartialEmojiConverter):
 
 class FindTxChl(Finder, TextChannelConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		return find(lambda chan: chan.name.lower().startswith(argument.lower()), ctx.guild.text_channels)
 
 class FindVcChl(Finder, VoiceChannelConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		return find(lambda chan: chan.name.lower().startswith(argument.lower()), ctx.guild.voice_channels)
 
 class FindChl(Finder, TextChannelConverter, VoiceChannelConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		return find(
 			lambda chl: chl.name.lower().startswith(argument.lower()), ctx.guild.text_channels + ctx.guild.voice_channels
 		)
 
 class FindRole(Finder, RoleConverter):
 	@staticmethod
-	def find(ctx, argument):
+	async def find(ctx, argument):
 		return lambda role: role.name.lower().startswith(argument.lower()), ctx.guild.roles
 
 class Queue:
