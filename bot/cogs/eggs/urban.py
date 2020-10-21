8092c8d921cf0a1cf49688d470af75acf96a96dc
tooru <sergunkoudrin@inbox.lv>
2020-10-21 08:47:33 +0300

    make use of text_reaction deco

--- bot/cogs/eggs/urban.py
+++ bot/cogs/eggs/urban.py
@@ -1,17 +1,15 @@
 from discord.ext.commands import Cog
 from re import search
-from .._utils import is_valid
+from .._utils import text_reaction
 
 class Urban(Cog, name="Eggs"):
 	def __init__(self, client):
 		self.bot = client
 
-	@Cog.listener("on_message")
+	@text_reaction(send=False, regex=r"^(?:wh?[ao]t(?:['’]?s|\sis)\s)((?:(?!up|good|with|it|this|that|so|the|about|goin|happenin).)*?)\??$", check=lambda msg: len(msg.content) < 30)
 	async def urban_listener(self, msg):
-		regex = r"^(?:wh?[ao]t(?:['’]?s|\sis)\s)((?:(?!up|good|with|it|this|that|so|the|about|goin|happenin).)*?)\??$"
-		if len(msg.content) < 30 and is_valid(self.bot, msg, regex):
-			term = search(regex, msg.content.lower()).group(1)
-			await self.bot.get_command("urban")(await self.bot.get_context(msg), term=term)
+		term = search(r"^(?:wh?[ao]t(?:['’]?s|\sis)\s)((?:(?!up|good|with|it|this|that|so|the|about|goin|happenin).)*?)\??$", msg.content.lower()).group(1)
+		await self.bot.get_command("urban")(await self.bot.get_context(msg), term=term)
 
 def setup(client):
 	client.add_cog(Urban(client))
