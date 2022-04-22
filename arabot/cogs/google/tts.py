0764bd81fbefe081625db03d0da9ae8c8aafe890
tooru <tooru@tooru.ml>
2022-04-22 17:48:40 +0300

    Add TTS language support

--- arabot/cogs/google/tts.py
+++ arabot/cogs/google/tts.py
@@ -1,10 +1,15 @@
+import datetime
 from base64 import b64decode
+from functools import partial
 from io import BytesIO
 
 from aiohttp import ClientResponseError
 from arabot.core import Ara, Category, Cog, Context
+from async_lru import alru_cache
 from disnake import File, PCMAudio
+from disnake.ext import tasks
 from disnake.ext.commands import BucketType, command, cooldown
+from disnake.utils import find
 
 
 class TextToSpeech(Cog, category=Category.GENERAL, keys={"g_tts_key"}):
@@ -12,24 +17,25 @@ class TextToSpeech(Cog, category=Category.GENERAL, keys={"g_tts_key"}):
 
     def __init__(self, ara: Ara):
         self.ara = ara
+        self._invalidate_voices_cache.start()
 
     @command(aliases=["synth"], brief="Synthesize speech from text")
     @cooldown(5, 60, BucketType.guild)
-    async def tts(self, ctx: Context, *, text: str = None):
-        if not text:
-            await ctx.send("I need text to synthesize")
-            return
-
+    async def tts(self, ctx: Context, *query):
         async with ctx.typing():
-            ogg = await self.text_to_audio(text)
+            langs = await self.voices()
+            lang, text = self.parse_query(query, langs)
+
+            if not text and not (text := await ctx.message.rsearch(ctx, "content")):
+                await ctx.send("I need text to synthesize")
+                return
+
+            ogg = await self.text_to_audio(text, lang)
             await ctx.reply(file=File(ogg, text + ".ogg"))
 
     @command(aliases=["pronounce"], brief="Pronounce text in voice channel")
     @cooldown(5, 60, BucketType.guild)
-    async def speak(self, ctx: Context, *, text: str = None):
-        if not text:
-            await ctx.send("I need text to pronounce")
-            return
+    async def speak(self, ctx: Context, *query):
         if ctx.guild.voice_client:
             await ctx.send("I'm already speaking")
             return
@@ -37,11 +43,45 @@ class TextToSpeech(Cog, category=Category.GENERAL, keys={"g_tts_key"}):
             await ctx.send("You're not connected to a voice channel")
             return
 
-        pcm = await self.text_to_audio(text, "LINEAR16")
+        async with ctx.typing():
+            langs = await self.voices()
+            lang, text = self.parse_query(query, langs)
+
+            if not text and not (text := await ctx.message.rsearch(ctx, "content")):
+                await ctx.send("I need text to pronounce")
+                return
+
+            pcm = await self.text_to_audio(text, lang, "LINEAR16")
         await ctx.author.voice.channel.connect_play_disconnect(PCMAudio(pcm))
 
-    async def text_to_audio(self, text: str, encoding="OGG_OPUS") -> BytesIO:
-        lang = await self.detect_language(text) or self.DEFAULT_LANGUAGE
+    def parse_query(self, query: str, langs: list[dict]) -> tuple[str | None, str | None]:
+        find_lang = partial(self.find_lang, langs=langs)
+        match query:
+            case []:
+                lang = None
+                text = None
+
+            case [text]:
+                if find_lang(text):
+                    lang = text
+                    text = None
+                else:
+                    lang = None
+
+            case [lang, *text]:
+                text = " ".join(text)
+                if not find_lang(lang):
+                    text = f"{lang} {text}"
+                    lang = None
+
+        return lang, text
+
+    @staticmethod
+    def find_lang(string: str, langs: list[dict]) -> str | None:
+        return find(lambda lng: lng["languageCodes"][0].split("-")[0] == string.lower(), langs)
+
+    async def text_to_audio(self, text: str, lang=None, encoding="OGG_OPUS") -> BytesIO:
+        lang = lang or await self.detect_language(text) or self.DEFAULT_LANGUAGE
         audio = await self.synthesize(text, lang, encoding)
         return BytesIO(audio)
 
@@ -61,6 +101,14 @@ class TextToSpeech(Cog, category=Category.GENERAL, keys={"g_tts_key"}):
         )
         return b64decode(data["audioContent"])
 
+    @alru_cache(cache_exceptions=False)
+    async def voices(self, language_code: str | None = None) -> list[dict]:
+        data: dict[str, list[dict]] = await self.ara.session.fetch_json(
+            "https://texttospeech.googleapis.com/v1/voices",
+            params={"key": self.g_tts_key, "languageCode": language_code or ""},
+        )
+        return data["voices"]
+
     async def detect_language(self, text: str) -> str | None:
         try:
             data: dict[str, dict[str, list[list[dict]]]] = await self.ara.session.fetch_json(
@@ -72,3 +120,7 @@ class TextToSpeech(Cog, category=Category.GENERAL, keys={"g_tts_key"}):
         else:
             lang = data["data"]["detections"][0][0]["language"]
             return lang if lang != "und" else None
+
+    @tasks.loop(time=datetime.time())
+    async def _invalidate_voices_cache(self):
+        self.voices.cache_clear()
