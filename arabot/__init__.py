9953fa77c913fddab8b5f292d45d9a971f53e562
tooru <tooru@tooru.ml>
2022-04-29 00:23:49 +0300

    Change `DEBUG` to `TESTING`

--- arabot/__init__.py
+++ arabot/__init__.py
@@ -2,7 +2,7 @@ from os import getenv as _getenv
 
 from dotenv import load_dotenv as _load_dotenv
 
-__version__ = "6.0.0-beta1"
+__version__ = "6.0.0-beta2"
 
 _load_dotenv()
-TESTING = bool(_getenv("debug"))
+TESTING = bool(_getenv("testing"))
