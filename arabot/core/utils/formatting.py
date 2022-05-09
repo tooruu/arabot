15d76535999c9eed26ad48055f8c968b806ed0e1
tooru <tooru@tooru.ml>
2022-05-09 14:51:35 +0300

    Implicitly convert to `str`

--- arabot/core/utils/formatting.py
+++ arabot/core/utils/formatting.py
@@ -1,43 +1,41 @@
-def bold(s: str) -> str:
-    return "**{}**".format(s.replace("*", r"\*"))
+import re as _re
+from typing import Any as _Any
 
 
-def underline(s: str) -> str:
-    return "__{}__".format(s.replace("_", r"\_"))
+def bold(s: _Any) -> str:
+    return "**{}**".format(str(s).replace("*", r"\*"))
 
 
-def italic(s: str) -> str:
-    return "*{}*".format(s.replace("*", r"\*"))
+def underline(s: _Any) -> str:
+    return "__{}__".format(str(s).replace("_", r"\_"))
 
 
-def strikethrough(s: str) -> str:
-    return "~~{}~~".format(s.replace("~", r"\~"))
+def italic(s: _Any) -> str:
+    return "*{}*".format(str(s).replace("*", r"\*"))
 
 
-def dsafe(s: str) -> str:
-    return (
-        s.replace("*", r"\*")
-        .replace("_", r"\_")
-        .replace("~", r"\~")
-        .replace("|", r"\|")
-        .replace("`", r"\`")
-    )
+def strikethrough(s: _Any) -> str:
+    return "~~{}~~".format(str(s).replace("~", r"\~"))
 
 
-def spoiler(s: str) -> str:
-    return "||{}||".format(s.replace("|", r"\|"))
+def dsafe(s: _Any) -> str:
+    return _re.sub(r"([*_~|`])", r"\\\1", str(s))
 
 
-def mono(s: str) -> str:
-    return "`{}`".format(s.replace("`", r"\`"))
+def spoiler(s: _Any) -> str:
+    return "||{}||".format(str(s).replace("|", r"\|"))
 
 
-def code(s: str, lang: str = "") -> str:
-    return "```{}\n{}\n```".format(lang, s.replace("```", "``\u200b`"))
+def mono(s: _Any) -> str:
+    return "`{}`".format(str(s).replace("`", r"\`"))
 
 
-def unping(s: str) -> str:
-    return s.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere")
+def code(s: _Any, lang: str = "") -> str:
+    return "```{}\n{}\n```".format(lang, str(s).replace("```", "``\u200b`"))
+
+
+def unping(s: _Any) -> str:
+    return str(s).replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere")
 
 
 codeblock = cb = code
