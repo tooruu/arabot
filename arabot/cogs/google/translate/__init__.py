b18e418cb4ce6f79d92bc9fd6104bd4823635c1e
Sourcery AI <>
2022-05-27 09:58:48 +0000

    'Refactored by Sourcery'

--- arabot/cogs/google/translate/__init__.py
+++ arabot/cogs/google/translate/__init__.py
@@ -96,9 +96,14 @@ class Translate(Cog, category=Category.LOOKUP):
 
     @staticmethod
     def find_lang(string: str, langs: list[LangCodeAndOrName]) -> LangCodeAndOrName | None:
-        if not string:
-            return None
-        return find(lambda lang: re.fullmatch("|".join(lang), string, re.IGNORECASE), langs)
+        return (
+            find(
+                lambda lang: re.fullmatch("|".join(lang), string, re.IGNORECASE),
+                langs,
+            )
+            if string
+            else None
+        )
 
     @staticmethod
     def format_lang(lang: LangCodeAndOrName) -> str:
