8d4d46cea0eff3e62d4fd476b73cec779d4946f6
tooru <tooru@tooru.ga>
2022-07-29 17:37:23 +0300

    Run migration before start

--- arabot/__init__.py
+++ arabot/__init__.py
@@ -1,10 +1,12 @@
 from os import getenv as _getenv
+from subprocess import run as _run
 
 from dotenv import load_dotenv as _load_dotenv
 
+_run(("prisma", "db", "push"), check=True)  # Migrate MongoDB
 from .core import Ara
 
-__version__ = "8.0.0-alpha1"
+__version__ = "8.0.0-alpha2"
 
 _load_dotenv()
 TESTING = bool(_getenv("testing"))
