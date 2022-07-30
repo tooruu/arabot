670721f5c317d310fb25b8d449a011d9c96f7e32
tooru <tooru@tooru.ga>
2022-07-30 17:45:55 +0300

    Don't run migration on Heroku It's run by Heroku via Procfile

--- arabot/__init__.py
+++ arabot/__init__.py
@@ -1,12 +1,15 @@
+# pylint: disable=wrong-import-position
 from os import getenv as _getenv
 from subprocess import run as _run
 
 from dotenv import load_dotenv as _load_dotenv
 
-_run(("prisma", "db", "push"), check=True)  # Migrate MongoDB
-from .core import Ara
-
-__version__ = "8.0.0-alpha2"
+__version__ = "8.0.0-alpha3"
 
 _load_dotenv()
 TESTING = bool(_getenv("testing"))
+
+if not _getenv("HEROKU_APP_ID"):
+    _run(("prisma", "db", "push"), check=True)
+
+from .core import Ara
