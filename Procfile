b2577c4aa7368280a6671128d3b40605f53d0a46
tooru <tooru@tooru.ga>
2022-08-09 12:42:18 +0300

    Run migration regardless of system

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker: python -OO -m arabot
-release: prisma db push
--- arabot/__init__.py
+++ arabot/__init__.py
@@ -4,12 +4,11 @@ from subprocess import run as _run
 
 from dotenv import load_dotenv as _load_dotenv
 
-__version__ = "8.0.0-rc1"
+__version__ = "8.0.0"
 
 _load_dotenv()
 TESTING = bool(_getenv("testing"))
 
-if not _getenv("HEROKU_APP_ID"):
-    _run(("prisma", "db", "push"), check=True)
+_run(("prisma", "db", "push"), check=True)
 
 from .core import Ara
