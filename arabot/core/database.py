f05540ad290afbf8f6b348971e72019c2ee61784
tooru <tooru@tooru.ga>
2022-11-14 03:39:45 +0200

    Convert snowflakes in DB from strings to integers

--- arabot/core/database.py
+++ arabot/core/database.py
@@ -5,14 +5,16 @@ from prisma import Prisma
 class AraDB(Prisma):
     @alru_cache(cache_exceptions=False)
     async def get_guild_prefix(self, guild_id: int) -> str | None:
-        return getattr(await self.prefix.find_unique({"guild_id": str(guild_id)}), "prefix", None)
+        # Snowflake is a ulong which is not supported by MongoDB so we convert it to signed long
+        guild = {"guild_id": guild_id - 2**64 // 2}
+        return row.prefix if (row := await self.prefix.find_unique(guild)) else None
 
     async def set_guild_prefix(self, guild_id: int, prefix: str) -> None:
-        identifier = {"guild_id": str(guild_id)}
+        guild = {"guild_id": guild_id - 2**64 // 2}
         await self.prefix.upsert(
-            identifier,
+            guild,
             {
-                "create": {"prefix": prefix} | identifier,
+                "create": {"prefix": prefix} | guild,
                 "update": {"prefix": prefix},
             },
         )
