75bfbbfab30f066da953dba6b7b05f30833d5c3f
tooru <tooru@tooru.ga>
2023-05-19 05:48:19 +0300

    Protect bot owner from getting killed

--- arabot/modules/waifus.py
+++ arabot/modules/waifus.py
@@ -201,7 +201,10 @@ class Waifus(Cog, category=Category.WAIFUS, metaclass=WaifuCommandsMeta):
         if method == self.wclient.nsfw and not ctx.channel.is_nsfw():
             await ctx.reply_(Waifus.NSFW_IN_SFW_CHANNEL, False)
             return
-        targets = list(dict.fromkeys(t for t in targets if t))
+        targets: list[disnake.Member] = list(dict.fromkeys(t for t in targets if t))
+        if ctx.author != ctx.ara.owner and ctx.ara.owner in targets and ctx.command.name == "kill":
+            targets.remove(ctx.ara.owner)
+            await ctx.author.timeout(duration=60, reason=ctx._("owner_kill_timeout_reason"))
         embed = await self.generate_embed(targets, method, ctx)
         await ctx.send_ping(embed=embed)
 
