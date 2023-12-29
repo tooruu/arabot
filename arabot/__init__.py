03935e3b15bb523511881eee34c2ba6279fb0a7f
tooru <tooru@tooru.ga>
2023-12-29 03:05:40 +0200

    Check if bot role exists before using it as prefix

--- arabot/__init__.py
+++ arabot/__init__.py
@@ -9,7 +9,7 @@ __all__ = [
     "TESTING",
 ]
 
-__version__ = "8.16.0"
+__version__ = "8.16.1"
 
 run(("prisma", "db", "push"), check=True)
 load_dotenv()
--- arabot/__main__.py
+++ arabot/__main__.py
@@ -17,9 +17,9 @@ def setup_logging() -> None:
 
 async def prefix_manager(ara: Ara, msg: disnake.Message) -> str | None:
     custom_prefix = await ara.db.get_guild_prefix(msg.guild.id) or ";"
-    pfx_pattern = (
-        rf"{re.escape(custom_prefix)}\s*|ara\s+|<@(?:!?{ara.user.id}|&{msg.guild.self_role.id})>\s*"
-    )
+    pfx_pattern = rf"{re.escape(custom_prefix)}\s*|ara\s+|<@!?{ara.user.id}>\s*"
+    if msg.guild.self_role:
+        pfx_pattern += rf"|<@&{msg.guild.self_role.id}>\s*"
     return (found := re.match(pfx_pattern, msg.content, re.IGNORECASE)) and found[0]
 
 
