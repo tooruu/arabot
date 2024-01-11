9a5b8f39955e4bc817760320e72771a6d727feaa
tooru <tooru@tooru.ga>
2024-01-11 06:47:45 +0200

    Refactor ;ping to use OOP approach with MPL

--- arabot/modules/ping.py
+++ arabot/modules/ping.py
@@ -1,14 +1,11 @@
 from collections import deque
 from io import BytesIO
 
+import matplotlib.pyplot as plt
 from disnake import File
 from disnake.ext.commands import command
 from disnake.ext.tasks import loop
-from matplotlib import use
-
-use("AGG")
-from matplotlib import pyplot as plt
-from matplotlib.ticker import MaxNLocator
+from matplotlib.axes import Axes
 
 from arabot.core import Ara, Category, Cog, Context
 from arabot.utils import I18N
@@ -19,6 +16,10 @@ class Ping(Cog, category=Category.META):
         self.ara = ara
         self.log = deque([0] * 60, 60)
         self.store.start()
+        self.fig, ax = plt.subplots(figsize=(3, 1), dpi=180, layout="constrained")
+        self.ax: Axes = ax
+        self.ax.tick_params("x", direction="in", labelbottom=False)
+        self.ax.tick_params(labelsize=4, length=3, width=0.5, pad=0.3)
 
     @loop(minutes=1)
     async def store(self):
@@ -32,35 +33,28 @@ class Ping(Cog, category=Category.META):
     async def ping(self, ctx: Context):
         self.plot_graph(ctx._)
         image = self.plt_to_file()
+        self.ax.clear()
         await ctx.send(ctx._("pong").format(f"{ctx.ara.latency * 1000:.0f}"), file=image)
 
     def plot_graph(self, _: I18N):
         y_padding = 5
         x, y = range(self.log.maxlen), self.log
-        ax = plt.subplots(figsize=(3, 1))[1]
-        plt.plot(x, y, linewidth=0.5)
-        plt.subplots_adjust(bottom=0.2, right=0.97)
-        plt.tick_params(axis="x", direction="in", labelbottom=False)
-        plt.tick_params(labelsize=4, length=3, width=0.5, pad=0.3)
-        plt.ylabel(_("y_label"), fontsize=7)
-        plt.xlabel(_("x_label"), fontsize=8)
-        ax.set_xlim(x[0], x[-1])
-        ax.set_ylim(min(set(y) - {0} or [y_padding]) - y_padding, max(y) + y_padding)
-        ax.get_yaxis().set_major_locator(MaxNLocator(integer=True, nbins=5))
-        plt.fill_between(x, y, color="cyan", alpha=0.45)
+        self.ax.set_ylabel(_("y_label"), fontsize=7)
+        self.ax.set_xlabel(_("x_label"), fontsize=8)
+        self.ax.set_ylim(min(set(y) - {0} or [y_padding]) - y_padding, max(y) + y_padding)
+        self.ax.set_xlim(0, self.log.maxlen - 1)
+        self.ax.fill_between(x, y, color="cyan", alpha=0.45)
+        self.ax.plot(x, y, linewidth=0.5)
 
-    @staticmethod
-    def plt_to_file() -> File:
+    def plt_to_file(self) -> File:
         buf = BytesIO()
-        plt.savefig(buf, dpi=180, format="png", transparent=False)
+        self.fig.savefig(buf)
         buf.seek(0)
-        file = File(buf, "ping.png")
-        plt.close()
-        del buf
-        return file
+        return File(buf, "ping.png")
 
     def cog_unload(self):
         self.store.cancel()
+        plt.close(self.fig)
 
 
 def setup(ara: Ara):
